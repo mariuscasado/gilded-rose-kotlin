a1f5c8aa380dec461d25c6ec53654eebc622d230
Ricard Osorio <celtric@gmail.com>
2017-09-29 16:03:27 +0200

    Create BackstagePass item

--- src/main/kotlin/katas/gildedrose/GildedRose.kt
+++ src/main/kotlin/katas/gildedrose/GildedRose.kt
@@ -1,6 +1,7 @@
 package katas.gildedrose
 
 import katas.gildedrose.items.AgedBrie
+import katas.gildedrose.items.BackstagePass
 
 class GildedRose(internal var items: Array<Item>) {
 
@@ -12,6 +13,7 @@ class GildedRose(internal var items: Array<Item>) {
 
     private fun item(item: Item) = when {
         item.name.equals("Aged Brie") -> AgedBrie(item)
+        item.name.equals("Backstage passes to a TAFKAL80ETC concert") -> BackstagePass(item)
         else -> TemporaryItem(item)
     }
 }
---
+++ src/main/kotlin/katas/gildedrose/items/BackstagePass.kt
@@ -0,0 +1,31 @@
+package katas.gildedrose.items
+
+import katas.gildedrose.Item
+import katas.gildedrose.TemporaryItem
+
+class BackstagePass(item: Item) : TemporaryItem(item) {
+
+    override fun update() {
+        if (item.quality < 50) {
+            item.quality = item.quality + 1
+
+            if (item.sellIn < 11) {
+                if (item.quality < 50) {
+                    item.quality = item.quality + 1
+                }
+            }
+
+            if (item.sellIn < 6) {
+                if (item.quality < 50) {
+                    item.quality = item.quality + 1
+                }
+            }
+        }
+
+        item.sellIn = item.sellIn - 1
+
+        if (item.sellIn < 0) {
+            item.quality = item.quality - item.quality
+        }
+    }
+}
