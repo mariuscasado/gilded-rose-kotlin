9684c74631b00accfe831108859f65a073219c3b
Ricard Osorio <celtric@gmail.com>
2017-09-29 16:13:03 +0200

    Remove Sulfuras as it's not a perishable item

--- src/main/kotlin/katas/gildedrose/GildedRose.kt
+++ src/main/kotlin/katas/gildedrose/GildedRose.kt
@@ -2,20 +2,19 @@ package katas.gildedrose
 
 import katas.gildedrose.items.AgedBrie
 import katas.gildedrose.items.BackstagePass
-import katas.gildedrose.items.Sulfuras
 
 class GildedRose(internal var items: Array<Item>) {
 
     fun updateQuality() {
         for (i in items.indices) {
-            item(items[i]).update()
+            perishableItem(items[i])?.update()
         }
     }
 
-    private fun item(item: Item) = when {
+    private fun perishableItem(item: Item) = when {
         item.name.equals("Aged Brie") -> AgedBrie(item)
         item.name.equals("Backstage passes to a TAFKAL80ETC concert") -> BackstagePass(item)
-        item.name.equals("Sulfuras, Hand of Ragnaros") -> Sulfuras(item)
+        item.name.equals("Sulfuras, Hand of Ragnaros") -> null
         else -> PerishableItem(item)
     }
 }
