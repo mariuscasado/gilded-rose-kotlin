18a3671e643b298871b0d6bb12de55f8614d60d6
Ricard Osorio <celtric@gmail.com>
2017-09-29 16:19:04 +0200

    Simplify if's

--- src/main/kotlin/katas/gildedrose/items/BackstagePass.kt
+++ src/main/kotlin/katas/gildedrose/items/BackstagePass.kt
@@ -9,16 +9,12 @@ class BackstagePass(item: Item) : PerishableItem(item) {
         if (item.quality < 50) {
             item.quality = item.quality + 1
 
-            if (item.sellIn < 11) {
-                if (item.quality < 50) {
-                    item.quality = item.quality + 1
-                }
+            if (item.sellIn < 11 && item.quality < 50) {
+                item.quality = item.quality + 1
             }
 
-            if (item.sellIn < 6) {
-                if (item.quality < 50) {
-                    item.quality = item.quality + 1
-                }
+            if (item.sellIn < 6 && item.quality < 50) {
+                item.quality = item.quality + 1
             }
         }
 
