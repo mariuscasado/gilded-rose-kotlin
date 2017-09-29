d682e7e44118acb228a02f9011aaed9f46b16e9f
Marius Casado <marius@viscaweb.com>
2017-09-29 15:59:55 +0200

    refactor: Joining ifs

--- src/main/kotlin/katas/gildedrose/GildedRose.kt
+++ src/main/kotlin/katas/gildedrose/GildedRose.kt
@@ -5,6 +5,7 @@ class GildedRose(internal var items: Array<Item>) {
     fun updateQuality() {
         for (i in items.indices) {
             val item = items[i]
+
             if (item.name != "Aged Brie" && item.name != "Backstage passes to a TAFKAL80ETC concert") {
                 if (item.quality > 0 && item.name != "Sulfuras, Hand of Ragnaros") {
                     decreaseQuality(item)
@@ -30,24 +31,20 @@ class GildedRose(internal var items: Array<Item>) {
             if (item.sellIn < 0) {
                 if (item.name != "Aged Brie") {
                     if (item.name != "Backstage passes to a TAFKAL80ETC concert") {
-                        if (item.quality > 0) {
-                            if (item.name != "Sulfuras, Hand of Ragnaros") {
-                                decreaseQuality(item)
-                            }
+                        if (item.quality > 0 && item.name != "Sulfuras, Hand of Ragnaros") {
+                            decreaseQuality(item)
                         }
                     } else {
                         item.quality = 0
                     }
-                } else {
-                    if (item.quality < 50) {
-                        increaseQuality(item)
-                    }
+                } else if (item.quality < 50) {
+                    increaseQuality(item)
                 }
             }
         }
     }
 
-    private fun decreaseSellIn(item: Item){
+    private fun decreaseSellIn(item: Item) {
         item.sellIn = item.sellIn - 1
     }
 
