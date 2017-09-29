94b171b3cead1555a76dd11520a260fbea9a0541
Ricard Osorio <celtric@gmail.com>
2017-09-29 16:31:37 +0200

    Extract decreaseQuality()

--- src/main/kotlin/katas/gildedrose/PerishableItem.kt
+++ src/main/kotlin/katas/gildedrose/PerishableItem.kt
@@ -2,16 +2,21 @@ package katas.gildedrose
 
 open class PerishableItem(val item: Item) {
 
-    val MAX_QUALITY = 50
+    private val MIN_QUALITY = 0
+    private val MAX_QUALITY = 50
 
     open fun update() {
-        if (item.quality > 0) {
-            item.quality--
-        }
+        decreaseQuality()
 
         item.sellIn--
 
-        if (item.sellIn < 0 && item.quality > 0) {
+        if (item.sellIn < 0) {
+            decreaseQuality()
+        }
+    }
+
+    private fun decreaseQuality() {
+        if (item.quality > MIN_QUALITY) {
             item.quality--
         }
     }
