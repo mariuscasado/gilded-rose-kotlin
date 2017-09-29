e32e6999f50ae0bd33bc3dd12e9f003a0f1e9b26
Ricard Osorio <celtric@gmail.com>
2017-09-29 16:07:44 +0200

    Create Sulfuras item

--- src/main/kotlin/katas/gildedrose/GildedRose.kt
+++ src/main/kotlin/katas/gildedrose/GildedRose.kt
@@ -2,6 +2,7 @@ package katas.gildedrose
 
 import katas.gildedrose.items.AgedBrie
 import katas.gildedrose.items.BackstagePass
+import katas.gildedrose.items.Sulfuras
 
 class GildedRose(internal var items: Array<Item>) {
 
@@ -14,6 +15,7 @@ class GildedRose(internal var items: Array<Item>) {
     private fun item(item: Item) = when {
         item.name.equals("Aged Brie") -> AgedBrie(item)
         item.name.equals("Backstage passes to a TAFKAL80ETC concert") -> BackstagePass(item)
+        item.name.equals("Sulfuras, Hand of Ragnaros") -> Sulfuras(item)
         else -> TemporaryItem(item)
     }
 }
---
+++ src/main/kotlin/katas/gildedrose/items/Sulfuras.kt
@@ -0,0 +1,11 @@
+package katas.gildedrose.items
+
+import katas.gildedrose.Item
+import katas.gildedrose.TemporaryItem
+
+class Sulfuras(item: Item) : TemporaryItem(item) {
+
+    override fun update() {
+        // Empty? Fishy...
+    }
+}
