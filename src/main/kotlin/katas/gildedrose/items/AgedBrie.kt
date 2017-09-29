91b0bab8d7c92c2095e4e1a4d16c939bbfc2a615
Ricard Osorio <celtric@gmail.com>
2017-09-29 15:58:55 +0200

    Import AgedBrie logic

--- src/main/kotlin/katas/gildedrose/items/AgedBrie.kt
+++ src/main/kotlin/katas/gildedrose/items/AgedBrie.kt
@@ -4,7 +4,16 @@ import katas.gildedrose.Item
 import katas.gildedrose.TemporaryItem
 
 class AgedBrie(item: Item) : TemporaryItem(item) {
+
     override fun update() {
-        super.update()
+        if (item.quality < 50) {
+            item.quality = item.quality + 1
+        }
+
+        item.sellIn = item.sellIn - 1
+
+        if (item.sellIn < 0 && item.quality < 50) {
+            item.quality = item.quality + 1
+        }
     }
 }
